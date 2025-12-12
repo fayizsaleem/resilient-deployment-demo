7cea214be67d9979a281a58029045fc060448f68
fayizsaleem <fayizsaleem@gmail.com>
2025-12-12 10:47:33 +0000

    remove accidently committed secrets

--- django-app/simpleapp/views.py
+++ django-app/simpleapp/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
 def health(request):
     # Return status OK (change to 500 later to simulate failure)
-    return JsonResponse({"status": "ok"}, status=200)
+    return JsonResponse({"status": "error"}, status=500)
 
